23511e9a52155a91b5982a37d04513ccd0c11510
unknown <hechengwang@hfepay.com>
2019-11-12 11:56:28 +0800

    asdsa

---
+++ 1.java
@@ -0,0 +1,27 @@
+[http]
+	sslverify = true
+	sslcainfo = c:/program files/git/mingw64/ssl/certs/ca-bundle.crt
+[user]
+	name = ""
+	email = ""
+
+
+
+[http]
+	sslverify = true
+	sslcainfo = c:/program files/git/mingw64/ssl/certs/ca-bundle.crt
+[user]
+	name = ""
+	email = ""
+[http]
+	sslverify = true
+	sslcainfo = c:/program files/git/mingw64/ssl/certs/ca-bundle.crt
+[user]
+	name = ""
+	email = ""
+[http]
+	sslverify = true
+	sslcainfo = c:/program files/git/mingw64/ssl/certs/ca-bundle.crt
+[user]
+	name = ""
+	email = ""
